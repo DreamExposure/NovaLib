e80d8605bd270e9b0e1ae067bb3fcdacc8255c62
NovaFox161 <novamaday@gmail.com>
2018-05-16 09:14:56 -0500

    Add 1.8.x support

---
+++ v1_8_R3/src/main/java/com/novamaday/novalib/nms/v1_8_R3/ActionBar.java
@@ -0,0 +1,19 @@
+package com.novamaday.novalib.nms.v1_8_R3;
+
+import com.novamaday.novalib.api.packets.IActionBar;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
+import net.minecraft.server.v1_8_R3.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class ActionBar implements IActionBar {
+    @Override
+    public void send(Player player, String message) {
+        IChatBaseComponent icbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + message + "\"}");
+        PacketPlayOutChat bar = new PacketPlayOutChat(icbc, (byte) 2);
+        //TODO: send custom event maybe
+
+
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(bar);
+    }
+}
---
+++ v1_8_R3/src/main/java/com/novamaday/novalib/nms/v1_8_R3/Ping.java
@@ -0,0 +1,19 @@
+package com.novamaday.novalib.nms.v1_8_R3;
+
+import com.novamaday.novalib.api.packets.IPing;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class Ping implements IPing {
+
+    /**
+     * Gets the current ping of the player.
+     *
+     * @param p The player whose ping is to be gotten.
+     * @return The current ping of the player in MS.
+     */
+    @Override
+    public int getPing(Player p) {
+        return ((CraftPlayer) p).getHandle().ping;
+    }
+}
